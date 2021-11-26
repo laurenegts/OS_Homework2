73918b50afbab434ead7ff32ce3cb0500ddc753f
Lauren Egts <lauren@x1lauren.lan>
2021-11-26 12:29:45 -0500

    wrote code for safety algorithm part of the banker's algorithm.  TBD if it actually works.

--- banker.cpp
+++ banker.cpp
@@ -34,18 +34,110 @@ void printIntVector(vector<int> toPrint) {
 	cout << "\n";
 }
 
+void print2DVector(vector<vector<int>> toPrint){
+	for( int i = 0; i < toPrint.size(); i++){
+		printIntVector(toPrint[i]);
+	}
+	cout << "\n";
+}
+
+bool compareNeedToAvailable(vector<int> needProcess, vector<int> work){
+	for( int i = 0; i < work.size(); i++){
+		if( needProcess[i] > work[i] ){
+			return false;
+		}
+	}
+	return true;
+}
+bool isSafeState(int numberOfProcesses, vector<int> available, vector<vector<int>> need, 
+		vector<vector<int>> allocation){
+
+	vector<int> work (available);
+	
+	vector<bool> finish (numberOfProcesses);
+	for(int i = 0; i < numberOfProcesses; i++) { finish[i] = false; }
+
+	bool stepTwoTrue = false;
+	for( int i = 0; i < numberOfProcesses; i++) {
+		// need of process i is <= work
+		if( finish[i] == false && compareNeedToAvailable(need[i], work) == true ){
+			break;
+		}
+		for(int j = 0; i < numberOfProcesses; j++){
+			for(int k = 0; k < work.size(); k++){ 
+				int temp = work[k] + allocation[j][k];
+				work[k] = temp;
+			}
+		}	
+		finish[i] = true;
+	}
+
+	for( int i = 0; i < finish.size(); i++){
+		if( finish[i] == false ){
+			return false;
+		}
+	}
+	return true;
+}
+
+
 int main() {
 	ifstream input("input.txt");
 	string line;
+
+	vector<int> available;
+	vector<vector<int>> allocation;
+	vector<vector<int>> max;
+	vector<vector<int>> need;
+
+	int numberOfProcesses; // n on the slides
+	int numberOfResources; // m on the slides
 
 	if(!input.is_open()){ return 1; }
 
 	
 	// special case for first line
 	getline(input, line);
-	vector<int> available = convertStringToIntVector(line);
+	available = convertStringToIntVector(line);
+	numberOfResources = available.size(); 
 	printIntVector(available);
-	// now need to iterate through the rest of the input.txt file
+
+
+	getline(input, line);
+	while( line != "Max:" ){
+		vector<int> temp = convertStringToIntVector(line);
+		allocation.push_back(temp);
+		getline(input, line);
+	}
+	numberOfProcesses = allocation.size();
+
+	print2DVector(allocation);
+
+	// allocation.size is the number of processes.  idk how to make it so we just read 
+	// cleanly to the end so instead i'm just reading the same number of times as we have
+	// processes because that's guarenteed to be the right number of lines.  because if it's
+	// not the right number of lines then the input file is messed up, so it should crash anyways.
+	// it's a feature, not a bug.
+	for(int i = 0; i < numberOfProcesses; i++){
+		getline(input, line);
+		vector<int> temp = convertStringToIntVector(line);
+		max.push_back(temp);
+	}
+
+	print2DVector(max);
+
+	// Create the need matrix
+	for( int i = 0; i < numberOfProcesses; i++){
+		vector<int> temp;
+		for( int j = 0; j < numberOfResources; j++){
+			int sub = max[i][j] - allocation[i][j];
+			temp.push_back(sub);
+		}
+		need.push_back(temp);
+	}
+
+	print2DVector(need);
+
 
 
 	input.close();
